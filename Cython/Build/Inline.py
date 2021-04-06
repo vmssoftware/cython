f2418403b915eb19d654d1a8fca2c58b716ad462
svorfolomeev <vorfol@yandex.ru>
2021-04-06 09:35:55 +0500

    shorten hash

--- Cython/Build/Inline.py
+++ Cython/Build/Inline.py
@@ -150,6 +150,9 @@ def _populate_unbound(kwds, unbound_symbols, locals=None, globals=None):
 
 def _inline_key(orig_code, arg_sigs, language_level):
     key = orig_code, arg_sigs, sys.version_info, sys.executable, language_level, Cython.__version__
+    if sys.platform == 'OpenVMS':
+        # OpenVMS has limited image name length
+        return hashlib.blake2s(_unicode(key).encode('utf-8'), digest_size=6).hexdigest()
     return hashlib.sha1(_unicode(key).encode('utf-8')).hexdigest()
 
 def cython_inline(code, get_type=unsafe_type,
